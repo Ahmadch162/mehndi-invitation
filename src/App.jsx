49dcf01409499a904943b7c9d25bfbc2488ca11d
Ahmad Chaudhary <ahmadchaudhary720@gmail.com>
2026-09-20 10:45:28 +0500

    remove discover from 1st section

--- src/App.jsx
+++ src/App.jsx
@@ -1334,7 +1334,7 @@ export default function App() {
           </div>
         </div>
 
-        <div className="scroll-cue">Discover</div>
+        
       </header>
 
       {/* =========================
